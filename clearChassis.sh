16309d04908e41c3c507ac88f587fec0390df6e9
Brightshine <brightshine.hsieh@gmail.com>
2015-09-02 11:13:09 +0800

    Add clear chassis command

---
+++ clearChassis.sh
@@ -0,0 +1,19 @@
+#                                                                                                                                                                                                       [1554/1864]
+# Given BMC IP, use ipmi raw 0x30 0x03 to clear chassis status
+
+USER='ADMIN'
+PASSWD='ADMIN'
+OEM_RAW_CMD='0x30 0x2D '
+
+echo "$#"
+if [  $# -lt 1 ]; then
+        echo " Usage : $0 IP "
+        exit 1
+fi
+
+IP=$1
+
+ipmitool -H ${IP} -U ${USER} -P ${PASSWD} raw 0x30 0x03
+
+[ $? -eq 0 ] && echo "success" || echo "failed"
+
